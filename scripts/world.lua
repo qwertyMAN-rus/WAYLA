824b17ab9461b0b4d86c3bcccee4133d67c5e574
qwertyMAN-rus <omich007lol@mail.ru>
2024-08-17 22:51:36 +0500

    Добавил тело игрока в игнор рейкаста

--- scripts/world.lua
+++ scripts/world.lua
@@ -3,9 +3,9 @@ local HUD = require('qWAYLA:viewer_hud')
 function on_world_tick()
     local cam = cameras.get("base:first-person")
     local dir = cam:get_front()
-    local start = vec3.add(cam:get_pos(), dir)
+    local start = cam:get_pos()
     local max_distance = 10
-    local raycast_result = entities.raycast(start, dir, max_distance)
+    local raycast_result = entities.raycast(start, dir, max_distance, player.get_entity())
     if not raycast_result then
         HUD.set_block_UI_visable(false)
         HUD.set_entity_UI_visable(false)
